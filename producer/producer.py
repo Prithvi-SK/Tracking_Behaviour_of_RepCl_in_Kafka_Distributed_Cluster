1c343dcd49261c118f187bbdbd9ae621b13efde5
Prithvi-SK <prithvi.sk.0509@gmail.com>
2025-02-05 15:56:17 +0530

    sendjson

--- producer/producer.py
+++ producer/producer.py
@@ -14,18 +14,26 @@
 
 from kafka import KafkaProducer, KafkaConsumer
 import time
+import json
 
 request_topic = 'requesttopic'
 response_topic = 'responsetopic'
 bootstrap_servers = 'kafka:9092'
 
 
-producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
-consumer = KafkaConsumer(response_topic, bootstrap_servers=bootstrap_servers, auto_offset_reset='earliest', group_id='test-group')
+producer = KafkaProducer(bootstrap_servers=bootstrap_servers,
+                         value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+consumer = KafkaConsumer(response_topic, 
+                         bootstrap_servers=bootstrap_servers, 
+                         auto_offset_reset='earliest', 
+                         group_id='test-group', 
+                         enable_auto_commit=True,
+                         value_deserializer=lambda v: json.loads(v.decode('utf-8')))
 
 # Send message to consumer
 message = time.strftime("%H:%M:%S",time.localtime())
-producer.send(request_topic, message.encode('utf-8'))
+message={"hello":"world"}
+producer.send(request_topic, value=message)
 producer.flush()
 print(f'Sent: {message}')
 time.sleep(5)
@@ -36,5 +44,11 @@ time.sleep(5)
 
 # Wait for response
 for msg in consumer:
-    print(f"Received: {msg.value.decode()}")
+    print(f"raw {msg.value}")
+    # try:
+    #     data = json.loads(msg.value.decode('utf-8'))
+    #     print(f"Received: {data}")
+    # except json.JSONDecodeError as e:
+    #     print(f"Skipping corrupted message: {e}")
+    # print(f"Received: {msg.value}")
     break  # Exit after receiving one response
